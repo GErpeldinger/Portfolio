c642fd758f1082851bb3e3f48c42c25f7ddb6715
Erpeldinger Guillaume <erpeldinger.g@free.fr>
2020-10-07 01:22:37 +0200

    rename folder

--- app/src/components/page/Contact.jsx
+++ app/src/components/page/Contact.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {array} from "prop-types";
+import Loading from "../Loading";
 
 const Contact = ({load}) => {
     const [loading, setLoading] = load
+
+    if (loading) {
+        return <Loading />
+    }
 
     return (
         <div className="Contact">
