0dccf5c9406fb38683666fdade4026e81066b0ee
Erpeldinger Guillaume <erpeldinger.g@free.fr>
2020-11-11 15:38:20 +0100

    fix bug function

--- assets/react/utils/functions.js
+++ assets/react/utils/functions.js
@@ -1,7 +1,6 @@
 import { APP_URL } from './constants';
 
 export function generateUrlImageResizer(width, height, path) {
-    let extension = path.match(/(?<=.)[a-z]*$/g)[0]
-    path = path.replace('.' + extension, '')
-    return `${APP_URL}/media/resize/${width}/${height}${path}/${extension}`
+    path = path.replace('.jpg', '/jpg')
+    return `${APP_URL}/media/resize/${width}/${height}${path}`
 }
