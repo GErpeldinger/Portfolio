cf1fa70f369c62cba8a0f8ecab7baea1f2a5117b
Erpeldinger Guillaume <erpeldinger.g@free.fr>
2020-11-17 23:21:35 +0100

    Redesign homepage

--- assets/react/routes/Project.jsx
+++ assets/react/routes/Project.jsx
@@ -24,13 +24,13 @@ const Project = () => {
         <div className="Project">
             <Link to={ROUTES.projects}><FontAwesomeIcon icon={faArrowCircleLeft}/> Revenir à la liste des projets</Link>
             <div>
-                <h1>{project.name}</h1>
-                <div>
-                    {project.github && <a href={project.github} target="_blank"><FontAwesomeIcon icon={faGithubSquare} size="2x"/></a>}
-                    {project.link && <a href={project.link} target="_blank"><FontAwesomeIcon icon={faExternalLinkSquareAlt} size="2x"/></a>}
-                </div>
                 <div className="jumbotron">
+                    <h1>{project.name}</h1>
                     {parser(project.description)}
+                    <div>
+                        {project.github && <a href={project.github} target="_blank"><FontAwesomeIcon icon={faGithubSquare} size="2x"/></a>}
+                        {project.link && <a href={project.link} target="_blank"><FontAwesomeIcon icon={faExternalLinkSquareAlt} size="2x"/></a>}
+                    </div>
                 </div>
                 <TagsList tags={project.tags}/>
                 <Carousel video={project.video} images={project.images}/>
